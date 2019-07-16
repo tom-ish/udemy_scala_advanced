8b3224e051f70c13a129e64ba106e09c99ab671d
Tomohiro Ishiwata <tomohiro.ishiwata@gmail.com>
2019-07-16 18:55:49 +0200

    Parallel Collection => Issue : scala version : 2.13 scala-collection-parallel is a separate JAR library to import

--- src/lectures/part3_concurrency/ParallelUtils.scala
+++ src/lectures/part3_concurrency/ParallelUtils.scala
@@ -1,5 +1,7 @@
 package lectures.part3_concurrency
 
+import scala.collection.parallel.CollectionConverters._
+
 /**
   * Created by Tomohiro on 16 juillet 2019.
   */
@@ -7,6 +9,7 @@ package lectures.part3_concurrency
 object ParallelUtils extends App {
 
   // 1 - Parallel Collections
-  
+  val parList = List(1,2,3).par
 
+  val aParVector = ParVector[Int](1, 2, 3)
 }
